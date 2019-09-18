072fa80afa2000e466346e1cf65b2e19124d6124
JabbaThePadd <joha.eriksson@gmail.com>
2019-09-18 19:16:29 +0200

    Labb 2 färdig samt ändringar som gjordes på labb 1 under förr labben

--- lab1-alarmclock/src/UpdateClock.java
+++ lab1-alarmclock/src/UpdateClock.java
@@ -7,40 +7,42 @@ public class UpdateClock implements Runnable{
 	ClockOutput out;
 	BigData bigData;
 	int count;
-	
+
 	public UpdateClock(ClockOutput out, BigData bigData) {
 		this.sem1 = sem1;
 		this.out = out;
 		this.bigData = bigData;
-		this.count = 0;
-		//test
-		
+		this.count = 0;		
 	}
-	
+
 	private int convertTime(long time) {
-		
+
 		String h = Long.toString( (time / (1000*3600) + 2) % 24);
 		String m = Long.toString((time % (1000*3600)) / (1000*60));
 		String s = Long.toString((time % (1000*60)) / (1000));
 		//System.out.println(h + ", " +  m + ", " + s);
 		return Integer.parseInt(h + m + s);
 	}
-	
+
 	public void run() {
-		
+
 		long nextStop = System.currentTimeMillis();
-			while(true) {
-				
-				nextStop = nextStop + 1000;
+		while(true) {
+
+			nextStop = nextStop + 1000;
+			try {
 				bigData.tic();
-				try {
-					Thread.sleep(nextStop - System.currentTimeMillis());
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
 			}
-		
+			try {
+				Thread.sleep(nextStop - System.currentTimeMillis());
+			} catch (InterruptedException e) {
+				throw new Error(e);
+			}
+
+		}
+
 	}
 }
--- lab2-factory/src/factory/controller/ToolController.java
+++ lab2-factory/src/factory/controller/ToolController.java
@@ -5,49 +5,116 @@ import factory.model.WidgetKind;
 import factory.swingview.Factory;
 
 public class ToolController {
-    private final DigitalSignal conveyor, press, paint;
-    private final long pressingMillis, paintingMillis;
-    
-    public ToolController(DigitalSignal conveyor,
-                          DigitalSignal press,
-                          DigitalSignal paint,
-                          long pressingMillis,
-                          long paintingMillis)
-    {
-        this.conveyor = conveyor;
-        this.press = press;
-        this.paint = paint;
-        this.pressingMillis = pressingMillis;
-        this.paintingMillis = paintingMillis;
-    }
+	private final DigitalSignal conveyor, press, paint;
+	private final long pressingMillis, paintingMillis;
+	private boolean isPressing, isPainting;
 
-    public void onPressSensorHigh(WidgetKind widgetKind) throws InterruptedException {
-        //
-        // TODO: you will need to modify this method
-        //
-        if (widgetKind == WidgetKind.BLUE_RECTANGULAR_WIDGET) {
-            conveyor.off();
-            press.on();
-            Thread.sleep(pressingMillis);
-            press.off();
-            Thread.sleep(pressingMillis);   // press needs this time to retract
-            conveyor.on();
-        }
-    }
+	public ToolController(DigitalSignal conveyor,
+			DigitalSignal press,
+			DigitalSignal paint,
+			long pressingMillis,
+			long paintingMillis)
+	{
+		this.conveyor = conveyor;
+		this.press = press;
+		this.paint = paint;
+		this.pressingMillis = pressingMillis;
+		this.paintingMillis = paintingMillis;
+		this.isPainting = false;
+		this.isPressing = false;
+	}
 
-    public void onPaintSensorHigh(WidgetKind widgetKind) throws InterruptedException {
-        //
-        // TODO: you will need to modify this method
-        //
-        if (widgetKind == WidgetKind.ORANGE_ROUND_WIDGET) {
-        	// TODO
-        }
-    }
-    
-    // -----------------------------------------------------------------------
-    
-    public static void main(String[] args) {
-        Factory factory = new Factory();
-        factory.startSimulation();
-    }
+	public synchronized void onPressSensorHigh(WidgetKind widgetKind) throws InterruptedException {
+
+		if (widgetKind == WidgetKind.BLUE_RECTANGULAR_WIDGET) {
+
+			pressStartHelp();
+			waitOutside(pressingMillis);            
+			pressOffHelp();            
+			waitOutside(pressingMillis);   // press needs this time to retract            
+			pressOffNotifyHelp();
+
+			while(isPainting) {
+				System.out.println("Waiting in painting");
+				wait();
+			}            
+			conveyor.on();
+		}
+	}
+
+
+	public synchronized void onPaintSensorHigh(WidgetKind widgetKind) throws InterruptedException {
+
+		if (widgetKind == WidgetKind.ORANGE_ROUND_WIDGET) {
+
+			paintStartHelp();            
+			waitOutside(paintingMillis);
+			paintOffHelp();            
+			waitOutside(paintingMillis);   // press needs this time to retract
+			paintOffNotifyHelp();            
+
+			while(isPressing) {
+				System.out.println("Waiting in painting");
+				wait();
+			}           
+			conveyor.on();
+		}
+	}
+
+
+	// ------------------------------ Help Method On -------------------------------
+	/** Helper method: sleep outside of monitor for ’millis’ milliseconds. */
+	private void waitOutside(long millis) throws InterruptedException {
+		
+		long timeToWakeUp = System.currentTimeMillis() + millis;
+		
+		while ( timeToWakeUp > System.currentTimeMillis() ) {
+			
+			long dt = timeToWakeUp - System.currentTimeMillis();/* number of milliseconds left */ 
+			//System.out.println("dt: " + dt);
+			
+			//if(dt > 0) {
+			wait(dt);
+			//System.out.println("inne i if");
+			//}
+			
+		}
+	}
+
+	private synchronized void pressStartHelp() {
+		conveyor.off();
+		press.on();
+		isPressing = true;
+	}
+
+	private synchronized void pressOffHelp() {
+		press.off();
+	}
+
+	private synchronized void pressOffNotifyHelp() {
+		isPressing = false;
+		notifyAll();
+	}
+
+	private synchronized void paintStartHelp() {
+		conveyor.off();
+		paint.on();
+		isPainting = true;
+	}
+
+	private synchronized void paintOffHelp() {
+		paint.off();
+	}
+
+	private synchronized void paintOffNotifyHelp() {
+		isPainting = false;
+		notifyAll();
+	}
+
+	// --------------------- Help Methods End ------------------------------
+
+	public static void main(String[] args) {
+		Factory factory = new Factory();
+		factory.startSimulation();
+	}
 }
